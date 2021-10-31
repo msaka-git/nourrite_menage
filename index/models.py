b6ab2a1aae99e90002672134fc7cdfc0a909036b
msaka-git <mufitsaka@gmail.com>
2021-10-31 16:04:43 +0100

    main structure

--- index/models.py
+++ index/models.py
@@ -25,36 +25,3 @@ class nourriture(models.Model):
 class shopping(models.Model):
     shopping_date = models.DateField()
     shopping_spent = models.FloatField(null=False)
-
-
-# interacting with models
-"""
-Ex. adding new customer into customer_liesure table
-from index.models import customer_liesure
-new_cust=customer_liesure()
-new_cust.customer='grubers'
-
-In [33]: new_cust.customer
-Out[33]: 'grubers'
-
-To save:
-new_cust.save()
-
-OR you can to save with:
-In [35]: other_cust=customer_liesure(customer="parc")
-
-In [36]: other_cust.save()
-
-In [37]: 
-
-For loop example:
-In [74]: data=customer_liesure.objects.all()
-
-In [75]: for i in data:
-    ...:     print(i.customer)
-    ...: 
-grubers
-parc
-
-
-"""
